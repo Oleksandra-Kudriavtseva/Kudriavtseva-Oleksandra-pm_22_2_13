53830d72301aebc1a4ea9620ded105f3d389d184
Oleksandra-Kudriavtseva <oleksandra.kudriavtseva.pm.2022@lpnu.ua>
2024-04-12 08:52:40 +0300

    add photo and previous code components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TopmenuComponent} from "./topmenu/topmenu.component";
+import {SidemenuComponent} from "./sidemenu/sidemenu.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {StatisticsComponent} from "./statistics/statistics.component";
+import {StatisticsGraphComponent} from "./statistics-graph/statistics-graph.component";
+import {CircularGraphComponent} from "./circular-graph/circular-graph.component";
+import {TicketsComponent} from "./tickets/tickets.component";
+import {UpdatesComponent} from "./updates/updates.component";
+import {StatusComponent} from "./status/status.component";
+import {BottomComponent} from "./bottom/bottom.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopmenuComponent, SidemenuComponent,DashboardComponent, StatisticsComponent, StatisticsGraphComponent,CircularGraphComponent, TicketsComponent, UpdatesComponent,StatusComponent, BottomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
